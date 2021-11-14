e6afb0ee5a35d7446fbe8a65baf8b700c0a8f8b9
Irini Pantopoulou <ipantopoulou@draxis.gr>
2021-11-14 19:39:09 +0200

    Error Modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredName, setEnteredName] = useState('')
+  const [enteredName, setEnteredName] = useState('');
   const [enteredUsername, setenteredUsername] = useState('');
   const [enteredAge, setenteredAge] = useState('');
+  const [error, setError] = useState('');
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 || enteredName.trim().length === 0) {
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredAge.trim().length === 0 ||
+      enteredName.trim().length === 0
+    ) {
+      setError({
+        title: 'Invllid input name',
+        message: 'Please enter a value',
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invllid input age',
+        message: 'Please enter a value age',
+      });
       return;
     }
 
-    console.log(enteredUsername);
-    console.log(enteredAge);
-    console.log(enteredName);
-    props.onAddUser(enteredName, enteredUsername,enteredAge)
+    props.onAddUser(enteredName, enteredUsername, enteredAge);
 
     //reset the form
     setenteredUsername('');
     setenteredAge('');
     setEnteredName('');
-
   };
   const nameChangeHandler = (event) => {
-        setEnteredName(event.target.value);
+    setEnteredName(event.target.value);
   };
 
   const usernameChangeHandler = (event) => {
@@ -39,30 +49,48 @@ const AddUser = (props) => {
     setenteredAge(event.target.value);
   };
 
+  const closeErrorModalHandler = () => {
+    setError(null);
+    console.log('on close handler');
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='name'>name</label>
-        <input id='name' type='text' value={enteredName} onChange={nameChangeHandler} />
-        <label htmlFor='username'>Username</label>
-        <input
-          id='username'
-          type='text'
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
+    <div>
+      {error && (
+        <ErrorModal
+          onCloseErrorModal={closeErrorModalHandler}
+          title={error.title}
+          message={error.message}
         />
-        <label htmlFor='age'>Age (Years)</label>
-        <input
-          id='age'
-          type='number'
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type='submit'>
-          Add User
-        </Button>
-      </form>
-    </Card>
+      )}
+
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='name'>name</label>
+          <input
+            id='name'
+            type='text'
+            value={enteredName}
+            onChange={nameChangeHandler}
+          />
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor='age'>Age (Years)</label>
+          <input
+            id='age'
+            type='number'
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
